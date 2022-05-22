79069d4e0bc0b1baf8d2511ab4d299e4e5a64cc3
Edwin Shen <edshen17@avenues.org>
2022-05-22 13:11:35 -0400

    remove unusued dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,9 @@ import generateSitemap from 'vite-ssg-sitemap'
 import Layouts from 'vite-plugin-vue-layouts'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import Markdown from 'vite-plugin-md'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
 import Inspect from 'vite-plugin-inspect'
-import Prism from 'markdown-it-prism'
-import LinkAttributes from 'markdown-it-link-attributes'
 import Unocss from 'unocss/vite'
-
-const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
 
 export default defineConfig({
   resolve: {
@@ -48,20 +43,6 @@ export default defineConfig({
       dts: 'src/components.d.ts',
     }),
     Unocss(),
-    Markdown({
-      wrapperClasses: markdownWrapperClasses,
-      headEnabled: true,
-      markdownItSetup(md) {
-        md.use(Prism)
-        md.use(LinkAttributes, {
-          matcher: (link: string) => /^https?:\/\//.test(link),
-          attrs: {
-            target: '_blank',
-            rel: 'noopener',
-          },
-        })
-      },
-    }),
     VueI18n({
       runtimeOnly: true,
       compositionOnly: true,
